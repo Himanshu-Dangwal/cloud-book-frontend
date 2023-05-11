5ba41a5528f3f04b3732645b7e08adc15176c2c0
Himanshu Dangwal <himanshudangwal99@gmail.com>
2023-05-11 15:49:36 +0530

    Updated

--- src/context/notes/NoteContext.js
+++ src/context/notes/NoteContext.js
@@ -4,7 +4,7 @@ export const NoteContext = createContext()
 
 export function NoteProvider(props) {
 
-    const HOST = "http://localhost:8080"
+    const HOST = "https://cloudb.onrender.com"
 
     const initialNotes = []
 
